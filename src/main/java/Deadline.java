01959c7682584e14a41e8318be2291214dea777f
MingHowLogic <97429865+MingHowLogic@users.noreply.github.com>
2023-01-27 11:01:03 +0800

    Create Deadline.java

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Deadline extends Task{
+    private LocalDateTime deadline;
+
+    public Deadline(String name, LocalDateTime deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
+
+    @Override
+    public String toLog() {
+        return "D" + super.toLog() + "| " + this.deadline + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: "
+                + this.deadline.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)) + ")";
+    }
+}
