420b7649046beeca77e1585d6e10aef52fe920fc
MingHowLogic <97429865+MingHowLogic@users.noreply.github.com>
2023-01-27 10:58:02 +0800

    Create Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,38 @@
+public class Task {
+    private boolean isMarked;
+    private String taskName;
+
+    public Task(String name) {
+        this.isMarked = false;
+        this.taskName = name;
+    }
+
+    public void markTask() {
+        this.isMarked = true;
+    }
+
+    public void unmarkTask() {
+        this.isMarked = false;
+    }
+
+    public String getName() {
+        return this.taskName;
+    }
+
+    public String toLog() {
+        if (this.isMarked) {
+            return (" | 1 | " + this.taskName);
+        } else {
+            return(" | 0 | " + this.taskName);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (this.isMarked) {
+            return ("[X] " + this.taskName);
+        } else {
+            return ("[ ] " + this.taskName);
+        }
+    }
+}
